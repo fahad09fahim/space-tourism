14f62ffd172a933a1d360ec0f696a8611acca4cb
unknown <anik.fahim07@gmail.com>
2023-07-09 17:49:35 +0600

    finished

--- src/component/Home/Header.jsx
+++ src/component/Home/Header.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./Header.css";
 import Logo from "../../assets/shared/logo.svg";
 import { NavLink } from "react-router-dom";
+import { BeakerIcon } from "@heroicons/react/24/solid";
 const Header = () => {
   return (
     <nav className="nav-bar">
       {/* logo icon */}
-      <div>
+      <div className="nav-icon">
         <img src={Logo} alt="" />
       </div>
       {/* rectangular */}
@@ -16,6 +17,22 @@ const Header = () => {
         <NavLink to="/destination">01 Destination</NavLink>
         <NavLink to="/crew">02 Crew</NavLink>
         <NavLink to="/technology">03 Technology</NavLink>
+      </div>
+      <div className="toggle-btn">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
       </div>
     </nav>
   );
