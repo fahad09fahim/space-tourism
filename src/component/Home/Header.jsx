cab240eea970c40b419c0a6a069524436cc66760
unknown <anik.fahim07@gmail.com>
2023-07-09 19:35:28 +0600

    mobile-btn-implement

--- src/component/Home/Header.jsx
+++ src/component/Home/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/shared/logo.svg";
 import { NavLink } from "react-router-dom";
-import { BeakerIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav className="nav-bar">
       {/* logo icon */}
@@ -12,27 +14,14 @@ const Header = () => {
       </div>
       {/* rectangular */}
       <p></p>
-      <div className="nav-link">
+      <div className={open ? "nav-link-mobile" : "nav-link"}>
         <NavLink to="/">00 Home</NavLink>
         <NavLink to="/destination">01 Destination</NavLink>
         <NavLink to="/crew">02 Crew</NavLink>
         <NavLink to="/technology">03 Technology</NavLink>
       </div>
-      <div className="toggle-btn">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
+      <div className="toggle-btn" onClick={() => setOpen(!open)}>
+        {open ? <XMarkIcon /> : <Bars3Icon />}
       </div>
     </nav>
   );
